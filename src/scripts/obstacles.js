17b30c64ee16d3718b2799c3d0ca055dc3506eaf
Naoko Sugawara <infoseek122@gmail.com>
2023-10-22 10:18:44 -0400

    added generating random obstacles

--- src/scripts/obstacles.js
+++ src/scripts/obstacles.js
@@ -8,20 +8,20 @@ export default class Obstacles {
         }
         this.velocity = 1;
 
-        this.obstacles = [
-            this.generateObstacle(50),
-            this.generateObstacle(100),
-            this.generateObstacle(150),
-            // this.randomObstacles(firstPipeDistance + CONSTANTS.PIPE_SPACING),
-            // this.randomObstacles(firstPipeDistance + (CONSTANTS.PIPE_SPACING * 2)),
-          ];
+        this.obstacles = [];
+
+        for (let i=0; i<3; i++) {
+            const randomDistance =  Math.floor(Math.random() * 1000) + 700;
+            // debugger 
+            this.obstacles.push(this.generateObstacle(randomDistance));
+        }
     }
 
     
     generateObstacle(x) {
         const obstacle = {
             left: x, 
-            right: 50 + x, 
+            // right: 50 + x, 
         };
         return obstacle;
     }
@@ -29,24 +29,25 @@ export default class Obstacles {
     moveObstacles() {
         this.eachObstacles (function(obstacle) {
             obstacle.left -= this.velocity
-            obstacle.right -= this.velocity
+            // obstacle.right -= this.velocity
         });
     
         // If a obstacles has left the screen, add a new one to the end.
-        if (this.obstacles[0].right <= 0) {
-            this.obstacles.shift();
-            const newX = this.obstacles[1].left;
-            this.obstacles.push(this.generateObstacle(newX));
-        }
+        // if (this.obstacles[0].right <= 0) {
+        //     this.obstacles.shift();
+        //     const newX = this.obstacles[1].left;
+        //     this.obstacles.push(this.generateObstacle(newX));
+        // }
     }
 
     drawObstacles(ctx){
         // debugger
         this.eachObstacles(function(obstacle) {
             ctx.fillStyle = "red";
+            // debugger
             ctx.fillRect(
                 obstacle.left, 
-                obstacle.right, 
+                450, 
                 50, 
                 50
             );
